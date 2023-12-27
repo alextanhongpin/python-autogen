88d131d01ea066cd312330b5a5edf2b4368aaafc
alextanhongpin <alextan220990@gmail.com>
2023-12-28 01:12:16 +0800

    chore: update implementation

---
+++ coding/plot_stock_price.py
@@ -0,0 +1,25 @@
+# filename: plot_stock_price_change.py
+import matplotlib.pyplot as plt
+import yfinance as yf
+
+# Get the stock data
+nvda = yf.Ticker("NVDA")
+tesla = yf.Ticker("TSLA")
+
+# Get the stock prices
+nvda_prices = nvda.history(period="ytd")["Close"]
+tesla_prices = tesla.history(period="ytd")["Close"]
+
+# Calculate the stock price change
+nvda_change = (nvda_prices[-1] - nvda_prices[0]) / nvda_prices[0] * 100
+tesla_change = (tesla_prices[-1] - tesla_prices[0]) / tesla_prices[0] * 100
+
+# Plot the stock price change
+plt.plot(nvda_prices.index, nvda_change, label="NVDA")
+plt.plot(tesla_prices.index, tesla_change, label="TSLA")
+plt.xlabel("Date")
+plt.ylabel("Stock Price Change (%)")
+plt.title("NVDA and TSLA Stock Price Change YTD")
+plt.legend()
+plt.show()
+plt.savefig("stock_price_change.png")
--- main.py
+++ main.py
@@ -2,10 +2,27 @@ from autogen import AssistantAgent, UserProxyAgent, config_list_from_json
 
 # Load LLM inference endpoints from an env variable or a file
 # See https://microsoft.github.io/autogen/docs/FAQ#set-your-api-endpoints
-# and OAI_CONFIG_LIST_sample
+# and OAI_CONFIG_LIST_sample.json
 config_list = config_list_from_json(env_or_file="OAI_CONFIG_LIST")
-# You can also set config_list directly as a list, for example, config_list = [{'model': 'gpt-4', 'api_key': '<your OpenAI API key here>'},]
-assistant = AssistantAgent("assistant", llm_config={"config_list": config_list})
-user_proxy = UserProxyAgent("user_proxy", code_execution_config={"work_dir": "coding"})
-user_proxy.initiate_chat(assistant, message="Plan the requirements for a new feature")
+assistant = AssistantAgent(
+    "assistant",
+    llm_config={
+        "cache_seed": 42,
+        "config_list": config_list,
+        "temperature": 0,
+    },
+)
+user_proxy = UserProxyAgent(
+    name="user_proxy",
+    human_input_mode="NEVER",
+    max_consecutive_auto_reply=10,
+    is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
+    code_execution_config={
+        "work_dir": "coding",
+        "use_docker": False,  # set to True or image name like "python:3" to use docker
+    },
+)
+user_proxy.initiate_chat(
+    assistant, message="Plot a chart of NVDA and TESLA stock price change YTD."
+)
 # This initiates an automated chat between the two agents to solve the task
